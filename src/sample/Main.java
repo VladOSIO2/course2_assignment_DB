3f3150972b533d4426710206cd71be53e77f063b
VladOSIO2 <vl4ds1mus@gmail.com>
2022-06-19 13:40:32 +0300

    main fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,8 @@
 package sample;
 
-import db.entities.queries.QuizQuery;
 import gui.SceneStarter;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Main extends Application {
 
